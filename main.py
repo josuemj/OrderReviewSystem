22df2a21e71f13d9fcaa62606ca5d25bfc8ccb37
josuemj <98498772+josuemj@users.noreply.github.com>
2025-04-19 18:43:45 -0600

    update deprecated fastApi to -> lifespan

--- main.py
+++ main.py
@@ -4,21 +4,29 @@ from pymongo.errors import PyMongoError
 from app.db.client import client
 from app.routes import review, restaurant, files, login
 from dotenv import load_dotenv
+from contextlib import asynccontextmanager
+
 load_dotenv()
 
-app = FastAPI()
-app.include_router(review.router)
-app.include_router(restaurant.router)
-app.include_router(files.router)
-app.include_router(login.router)
-
-@app.on_event("startup")
-async def startup_db_check():
+# start up logic
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     try:
         await client.admin.command("ping")
         print("✅ Conexión a MongoDB Atlas exitosa.")
     except Exception as e:
         print("❌ Error de conexión a MongoDB:", e)
+
+    yield  
+    print("App shutting down... (you can clean resources here)")
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(review.router)
+app.include_router(restaurant.router)
+app.include_router(files.router)
+app.include_router(login.router)
+
 
 @app.get("/")
 async def root():
