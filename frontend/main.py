a1e94be46ff67ec80368e58b7278a7f9d4c7a9cc
josuemj <98498772+josuemj@users.noreply.github.com>
2025-04-20 20:28:14 -0600

    chore: sketched admin panel

--- frontend/main.py
+++ frontend/main.py
@@ -7,23 +7,42 @@ init_session()
 
 def main():
     if st.session_state.logged_in:
-        # Solo mostrar sidebar si está logueado
-        st.sidebar.title("🍕 Navegación")
-        selected = st.sidebar.radio("Ir a:", ["🏠 Home", "📦 Órdenes", "📝 Reseñas", "👤 Perfil"])
+        if st.session_state.user["name"] != "admin":
+            # Solo mostrar sidebar si está logueado
+            st.sidebar.title("🍕 Navegación")
+            selected = st.sidebar.radio("Ir a:", ["🏠 Home", "📦 Órdenes", "📝 Reseñas", "👤 Perfil"])
 
-        if selected == "🏠 Home":
-            home.render()
-        elif selected == "📦 Órdenes":
-            orders.render()
-        elif selected == "📝 Reseñas":
-            reviews.render()
-        elif selected == "👤 Perfil":
-            profile.render()
+            if selected == "🏠 Home":
+                home.render()
+            elif selected == "📦 Órdenes":
+                orders.render()
+            elif selected == "📝 Reseñas":
+                reviews.render()
+            elif selected == "👤 Perfil":
+                profile.render()
 
-        if st.sidebar.button("Cerrar sesión", key="logout_sidebar"):
-            st.session_state.logged_in = False
-            st.session_state.user = None
-            st.rerun()
+            if st.sidebar.button("Cerrar sesión", key="logout_sidebar"):
+                st.session_state.logged_in = False
+                st.session_state.user = None
+                st.rerun()
+        else:
+            
+            selected = st.sidebar.radio("Ir a:", ["🏠 Restaurantes", "📦 Órdenes", "📝 Stats"])
+
+            if selected == "🏠 Restaurantes":
+                st.title("Gestionar restaurantes")
+                st.text("CRUD restaurante")
+            elif selected == "📦 Órdenes":
+                st.title("Gestor de ordenes")
+                st.text("Cambiar de estado ordenes")
+            elif selected == "📝 Stats":
+                st.title("Stats")
+                st.text("Stas ventas platillos etc")
+                
+            if st.sidebar.button("Cerrar sesión", key="logout_sidebar"):
+                st.session_state.logged_in = False
+                st.session_state.user = None
+                st.rerun()
     else:
         # No mostrar sidebar si no ha iniciado sesión
         login.render()
